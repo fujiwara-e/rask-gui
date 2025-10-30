3e4a4c7f23d9a292b39d4ab0f6503d6e88865acf
fujiwara-e <pjxk6clf@s.okayama-u.ac.jp>
2025-10-30 23:31:21 +0900

    style: standardize quotes and format in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:3000",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
   },
   build: {
     manifest: true,
     rollupOptions: {
-      input: "/src/main.tsx",
+      input: '/src/main.tsx',
     },
   },
 })
